8d7d470ee2c8f812e8fafcce69ba4ad4ae135895
CarolineNeris <carolnerisfonseca@gmail.com>
2024-03-13 13:49:32 -0300

    fix-program

--- Aladdin/Program.cs
+++ Aladdin/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using LampadaClass;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma lâmpada inicialmente ligada
+            Lampada lampada = new Lampada(true);
+
+            // Imprimindo o estado inicial da lâmpada
+            Console.WriteLine("Estado inicial da lâmpada:");
+            lampada.Imprimir();
+
+            // Desligando a lâmpada e imprimindo o estado
+            lampada.Desligar();
+            Console.WriteLine("\nLâmpada desligada:");
+            lampada.Imprimir();
+
+            // Ligando a lâmpada novamente e imprimindo o estado
+            lampada.Ligar();
+            Console.WriteLine("\nLâmpada ligada novamente:");
+            lampada.Imprimir();
+
+            // Aguardando o usuário pressionar uma tecla para fechar o programa
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
