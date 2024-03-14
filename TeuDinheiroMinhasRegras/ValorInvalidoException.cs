16026f1866833b922f290d06aac87d0bc85642ae
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-14 20:34:18 -0300

    create exception valor invelido

---
+++ TeuDinheiroMinhasRegras/ValorInvalidoException.cs
@@ -0,0 +1,13 @@
+﻿using System.Dynamic;
+
+namespace TeuDinheiroMinhasRegras.Exeception;
+
+public class ValorInvalidoException: Exception
+{
+    public double Valor { get;}
+
+    public ValorInvalidoException(string message, double valor):base (message){
+        Valor=valor;
+    }
+
+}
