9ca07d35c960bad4e716bcfbb22f5799dc143b3c
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-14 19:29:10 -0300

    fix-MetodoImprimir

--- Aladdin/DataClass.cs
+++ Aladdin/DataClass.cs
@@ -8,6 +8,9 @@ class Data{
     private readonly int _hora;
     private readonly int _minuto;
     private readonly int _segundo;
+
+    public const int FORMATO_12H = 12;
+    public const int FORMATO_24H = 24;
 //primeiro construtor
 public Data (int dia, int mes, int ano) {
     _dia = dia;
@@ -19,7 +22,7 @@ public Data (int dia, int mes, int ano) {
     {
         //verificar hora formato de 0 a 23 (adicionalmente segundos e minutos)
 
-        if (hora<0||hora<=23){
+        if (hora<0||hora>23){
             throw new ArgumentOutOfRangeException(nameof(hora), "A hora deve estar entre 0 e 23.");
 
         }
@@ -30,7 +33,46 @@ public Data (int dia, int mes, int ano) {
         if (segundo<0 ||segundo>61){
             throw new ArgumentOutOfRangeException(nameof(segundo), "O segundo deve estar entre 0 e 60");
         }
+
+        _hora=hora;
+        _minuto=minuto;
+        _segundo=segundo;
     }
+
+    public int Dia => _dia;
+    public int Mes => _mes;
+    public int Ano => _ano;
+    public int Hora => _hora;
+    public int Minuto => _minuto;
+    public int Segundo => _segundo;
+
+
+    public void Imprimir(int formatoHora)
+    {
+        
+        string formato;
+        if (formatoHora == FORMATO_12H)
+        {
+            formato = "dd/MM/yyyy hh:mm:ss tt"; // Formato de 12 horas
+        }
+        else if (formatoHora == FORMATO_24H)
+        {
+            formato = "dd/MM/yyyy HH:mm:ss"; // Formato de 24 horas
+        }
+        else
+        {
+            throw new ArgumentException("Formato de hora inválido. Use FORMATO_12H ou FORMATO_24H.");
+        }
+
+        if (_hora == 0 && _minuto == 0 && _segundo == 0)
+        {
+            formato = "dd/MM/yyyy"; 
+        }
+
+        DateTime dataHora = new DateTime(Ano, Mes, Dia, Hora, Minuto, Segundo);
+        Console.WriteLine(dataHora.ToString(formato));
+    }
+
 }
 
 
