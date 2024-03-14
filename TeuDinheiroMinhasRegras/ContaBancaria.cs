373baa15c16e4e6cf9c49636fdd09c947429fe6f
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-14 20:07:31 -0300

    implementação inicial da classe

---
+++ TeuDinheiroMinhasRegras/ContaBancaria.cs
@@ -0,0 +1,28 @@
+﻿namespace TeuDinheiroMinhasRegras.ContaBancaria;
+
+public class ContaBancaria
+{
+    private double saldo;
+    
+
+    public ContaBancaria (double saldoInicial){
+        saldo=saldoInicial;
+    }
+
+    public void Sacar (double valorSaque){
+
+        saldo-=valorSaque;
+
+    }
+
+    public void Depositar (double valorDeposito){
+        saldo+=valorDeposito;
+    }
+
+    public void Transferir(double valorTransferencia, ContaBancaria contaDestino)
+        {
+            saldo -= valorTransferencia;
+            contaDestino.Depositar(valorTransferencia);
+        }
+
+}
