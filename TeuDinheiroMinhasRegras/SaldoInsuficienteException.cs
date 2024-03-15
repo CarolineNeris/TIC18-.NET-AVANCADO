8491932bad95a9526d31f231b5bb46ba26eca9a5
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-14 21:11:16 -0300

    fix exeception

---
+++ TeuDinheiroMinhasRegras/SaldoInsuficienteException.cs
@@ -0,0 +1,11 @@
+﻿namespace TeuDinheiroMinhasRegras.Exeception;
+
+public class SaldoInsuficienteException:Exception
+{
+    public double Valor {get;}
+
+    public SaldoInsuficienteException ( double valor): base("Saldo insuficiente para realizar a operação"){
+        Valor = valor;
+    }
+
+}
--- TeuDinheiroMinhasRegras/ValorInvalidoException.cs
+++ TeuDinheiroMinhasRegras/ValorInvalidoException.cs
@@ -6,7 +6,7 @@ public class ValorInvalidoException: Exception
 {
     public double Valor { get;}
 
-    public ValorInvalidoException(string message, double valor):base (message){
+    public ValorInvalidoException( double valor):base ("Saldo insuficiente para realizar a operação."){
         Valor=valor;
     }
 
