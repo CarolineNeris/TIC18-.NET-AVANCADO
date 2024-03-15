894771c9a859c664ef3825d622eea7858c02d2fc
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-14 23:42:55 -0300

    create exercicio

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+enum Exercicio
+{
+    Funcional = 1,
+    Cardio = 2,
+    Musculacao = 3,
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(" exercícios:");
+            foreach (Exercicio exercicio in Enum.GetValues(typeof(Exercicio)))
+            {
+                Console.WriteLine($"Nome: {exercicio}, Valor: {(int)exercicio}");
+            }
+
+            Console.WriteLine("Escolha um exercicio pelo numero:");
+            int input = int.Parse(Console.ReadLine());
+
+            if (Enum.IsDefined(typeof(Exercicio), input))
+            {
+                Exercicio exercicioEscolhido = (Exercicio)input;
+                Console.WriteLine($"Exercicio escolhidoo: {exercicioEscolhido}");
+            }
+            else
+            {
+                Console.WriteLine("Erro: Exercício inválido. escolha outra opção.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Valor inválido. Certifique-se de digitar um número inteiro.");
+        }
+    }
+}
+
+
+
