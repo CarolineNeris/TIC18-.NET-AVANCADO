7e1b37d02fbd5563696c723324e2338f3075b0dc
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-17 22:42:56 -0300

    feito

--- Pratica_03/Program.cs
+++ Pratica_03/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        List<int> numerosInteiros = new List<int> { 3, 7, 2, 4, 6 };
+        List<double> numerosDouble = numerosInteiros.ConvertAll(x => (double)x / 2);
+        foreach (double numeros in numerosDouble)
+        {
+            Console.WriteLine(numeros);
+        }
+
+    }
+}
