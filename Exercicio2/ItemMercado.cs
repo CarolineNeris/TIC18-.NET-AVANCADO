9a7b5f95a0bfe990c511097b69a35d9e1fc2a6d5
Ana Caroline <carolnerisfonseca@gmail.com>
2024-03-18 00:27:37 -0300

    exercicio 2 pronto

---
+++ Exercicio2/ItemMercado.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio2.ItemMercado;
+
+public class ItemMercado
+{
+    public string Nome { get; set; }
+
+    public double Preco { get; set; }
+
+    public Tipos Tipo { get; set; }
+
+    public ItemMercado(string nome, Tipos tipo, double preco)
+    {
+        Nome=nome;
+        Preco=preco;
+        Tipo=tipo;
+    }
+
+
+}
+
+public enum Tipos
+{
+
+    Comida,
+    Bebida,
+    Higiene,
+    Limpeza
+
+}
